0332f01190be8dc650f5a6d084aac72fb9b197ed
jige <1>
2020-08-10 21:11:24 +0800

    "配置文件读取，logback日志系统"

--- src/main/java/com/hqyj/javaSpringBoot/test/Test.java
+++ src/main/java/com/hqyj/javaSpringBoot/test/Test.java
@@ -1,5 +1,10 @@
 package com.hqyj.javaSpringBoot.test;
 
+import com.hqyj.javaSpringBoot.applicationTest.TestApp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +13,61 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/test")
 public class Test {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Test.class);
+    @Value("${server.port}")
+    private int port;
+    @Value("${com.name}")
+    private String name;
+    @Value("${com.age}")
+    private int age;
+    @Value("${com.desc}")
+    private String desc;
+    @Value("${com.random}")
+    private String random;
+
+    @Autowired
+    private TestApp testApp;
+
+    /*
+       127.0.0.1:8085/test/logTest------------get
+    */
+    @GetMapping("/logTest")
+    @ResponseBody
+    public String logTest() {
+        LOGGER.trace("this is trace test");
+        LOGGER.debug("this is debug test");
+        LOGGER.info("this is info test");
+        LOGGER.warn("this is warn test");
+        LOGGER.error("this is error test");
+        return "this is log test!";
+    }
+
+    /*
+        127.0.0.1:8085/test/config------------get
+     */
+    @GetMapping("/config")
+    @ResponseBody
+    public String testApplication() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(port).append("-----")
+                .append(name).append("-----")
+                .append(age).append("-----")
+                .append(desc).append("-----")
+                .append(random).append("-----").append("<br>");
+        sb.append(testApp.getPort()).append("-----")
+                .append(testApp.getName()).append("-----")
+                .append(testApp.getAge()).append("-----")
+                .append(testApp.getDesc()).append("-----")
+                .append(testApp.getRandom()).append("-----");
+        return sb.toString();
+    }
+
     /*
         127.0.0.1:8080/test/testSpringBoot------------get
      */
     @GetMapping("/testSpringBoot")
     @ResponseBody
-    public String testSpringBoot(){
+    public String testSpringBoot() {
         return "I hava a dream!";
     }
 }
